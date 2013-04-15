e936fc65050a983d00061198f4bb5088d60baf88
Chris Barrett <chris.d.barrett@me.com>
2013-04-15 14:12:46 +1200

    Change symbol prefix -> ertml

--- ert-modeline.el
+++ ert-modeline.el
@@ -1,4 +1,4 @@
-;;; ert-modeline-mode --- displays ert test results in the modeline.
+;;; ert-modeline --- displays ert test results in the modeline.
 
 ;; Copyright (C) 2013 Chris Barrett
 
@@ -32,20 +32,25 @@
 
 (defgroup ert-modeline-mode nil
   "Runs ert tests while you edit and displays the results in the modeline."
-  :prefix "ebg-"
+  :prefix "ertml-"
   :group 'tools)
 
-(defface ebg-failing-face
+(defcustom ertml-selector t
+  "The selector to use for running ERT tests."
+  :group 'ert-modeline
+  :type 'symbol)
+
+(defface ertml-failing-face
   '((t :inherit error))
   "Face for error indicator."
   :group 'ert-modeline-mode)
 
-(defface ebg-warning-face
+(defface ertml-warning-face
   '((t :inherit warning))
   "Face for warning indicator."
   :group 'ert-modeline-mode)
 
-(defface ebg-passing-face
+(defface ertml-passing-face
   '((t (:foreground "green")))
   "Face for passing tests indicator."
   :group 'ert-modeline-mode)
@@ -53,42 +58,42 @@
 ;;; ----------------------------------------------------------------------------
 ;;; Mode functions
 
-(defvar ebg--status-text " [ert]"
+(defvar ertml--status-text " [ert]"
   "The string to use to represent the current status in the modeline.")
 
 ;;;###autoload
 (define-minor-mode ert-modeline-mode
   "Displays the current status of ERT tests in the modeline."
   :init-value nil
-  :lighter (:eval ebg--status-text)
+  :lighter (:eval ertml--status-text)
 
   (cond
    (ert-modeline-mode
-    (ebg--run-tests)
-    (add-hook 'after-save-hook 'ebg--run-tests nil t))
+    (ertml--run-tests)
+    (add-hook 'after-save-hook 'ertml--run-tests nil t))
 
    (t
-    (remove-hook 'after-save-hook 'ebg--run-tests t))))
+    (remove-hook 'after-save-hook 'ertml--run-tests t))))
 
-(defun ebg--run-tests (&rest _)
+(defun ertml--run-tests (&rest _)
   "Run ERT in the modeline and update the modeline."
   ;; Rebind `message' so that we do not see printed results.
   (flet ((message (&rest _)))
-    (setq ebg--status-text (ebg--summarize (ert-run-tests-batch t)))))
+    (setq ertml--status-text (ertml--summarize (ert-run-tests-batch ertml-selector)))))
 
-(defun ebg--summarize (results)
+(defun ertml--summarize (results)
   "Select a circle corresponding to the type and number of RESULTS."
   (let ((failing (ert--stats-failed-unexpected results)))
     (cond
      ;; No tests are enabled.
      ((>= 0 (length (ert--stats-tests results)))
-      (propertize " [ert]" 'font-lock-face 'ebg-warning-face))
+      (propertize " [ert]" 'font-lock-face 'ertml-warning-face))
      ;; Indicate number of failing tests.
      ((< 0 failing)
-      (propertize (format " [%s]" failing) 'font-lock-face 'ebg-failing-face))
+      (propertize (format " [%s]" failing) 'font-lock-face 'ertml-failing-face))
      ;; Show OK for all passing.
      (t
-      (propertize " [OK]" 'font-lock-face 'ebg-passing-face)))))
+      (propertize " [OK]" 'font-lock-face 'ertml-passing-face)))))
 
 ;;; ----------------------------------------------------------------------------
 ;;; Eval advice
@@ -101,15 +106,15 @@
               eval-current-buffer
               eval-region)
             )
-  (eval `(defadvice ,fn (after ebg--run activate)
+  (eval `(defadvice ,fn (after ertml--run activate)
            (when (and (boundp 'ert-modeline-mode) ert-modeline-mode)
-             (ebg--run-tests)))))
+             (ertml--run-tests)))))
 
-(provide 'ert-modeline-mode)
+(provide 'ert-modeline)
 
 ;; Local Variables:
 ;; lexical-binding: t
 ;; byte-compile-warnings: (not obsolete)
 ;; End:
 
-;;; ert-modeline-mode.el ends here
+;;; ert-modeline.el ends here
